d735031b89345e7f07ee4a56dbaa7ece94687c08
Andrei Rus <rus.andrey93@outlook.com>
2020-04-16 19:49:59 +0300

    Curs 4 - Implementare movment

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # tetris
-Build tetris game in JS
+1. Create grid class.
+2. Rewrite the code using module type for scripts. Talk about dependencies.
+3. Create one model(L shape) and draw it on the grid.
+4. Implement events (keydown event - up, left, right,down)
+5. Animate the shape with setInterval.
+
+6. Create another shape and draw it to the grid, add movement for it.
+7. Create Shape class and achieve inheritance.
--- main.js
+++ main.js
@@ -1,32 +1,64 @@
-const canvas = document.getElementById('canvasId');
-const context = canvas.getContext("2d");
+import { Grid } from './grid.js';
+import { L } from './shapes/l-shape.js';
+// import { O } from './shapes/o-shape.js';
 
 const rows = 20;
 const columns = 10;
-const width = 30;
-const height = 30;
 
-let grid = [];
+const grid = new Grid(rows, columns);
+grid.create();
+grid.draw();
 
-const createGrid = () => {
-    for (let row = 0; row < rows; row++) {
-        grid[row] = [];
-        for (let column = 0; column < columns; column++) {
-            grid[row][column] = new Cell(row, column, row % 2 ? 'red' : 'blue');
-        }
+//TODO - why 0.
+const lShape = new L(0, 3, grid.cells);
+lShape.draw();
+
+// const oShape = new O(5, 3, grid.cells);
+// oShape.draw();
+
+document.addEventListener("keydown", event => {
+    switch (event.key) {
+        case 'ArrowUp':
+            grid.draw();
+            lShape.moveUp();
+            lShape.draw();
+            break;
+        case 'ArrowDown':
+            grid.draw();
+            lShape.moveDown();
+            lShape.draw();
+            break;
+        case 'ArrowLeft':
+            grid.draw();
+            lShape.moveLeft();
+            lShape.draw();
+            break;
+        case 'ArrowRight':
+            grid.draw();
+            lShape.moveRight();
+            lShape.draw();
+            break;
     }
-}
+});
 
-const drawGrid = () => {
-    for (let row = 0; row < rows; row++) {
-        for (let column = 0; column < columns; column++) {
-            grid[row][column].draw();
-        }
-    }
-}
+setInterval(() => {
+    grid.draw();
+    lShape.moveDown();
+    lShape.draw();
+}, 500);
 
-createGrid();
-drawGrid();
+// function writeText(text, callback) {
+//     console.log('Inainte de afisare');
+//     console.log(text);
+//     console.log('Dupa de afisare');
+//     callback();
+// }
+
+// function writeEnd() {
+//     console.log('The end.');
+// }
+
+// writeText('Salut!', writeEnd);
 
 
 
